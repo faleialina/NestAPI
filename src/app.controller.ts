d43d4f37aa4537776965d25b1312eb5dd0b0cce1
faleialina <august3@mail.ru>
2025-02-06 22:19:28 +0300

    fix controller and service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,24 +9,52 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface iUser {
+  name: string;
+  description: string;
+}
+
+interface iUserWithId extends iUser {
+  id: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getAllItem(): string {
-    return this.appService.getAllItem();
+  getAllItem(): iUserWithId[] | string {
+    try {
+      return this.appService.getAllItem();
+    } catch (error) {
+      return (error as Error).message;
+    }
   }
   @Post()
-  createItem(@Body() obj: any): any {
-    return this.appService.createItem(obj);
+  createItem(@Body() obj: iUser): iUserWithId[] | string {
+    try {
+      return this.appService.createItem(obj);
+    } catch (error) {
+      return (error as Error).message;
+    }
   }
   @Put(':id')
-  updateItem(@Body() obj: any, @Param('id') id: string): any {
-    return this.appService.updateItem(id, obj);
+  updateItem(
+    @Body() obj: iUser,
+    @Param('id') id: string,
+  ): iUserWithId[] | string {
+    try {
+      return this.appService.updateItem(id, obj);
+    } catch (error) {
+      return (error as Error).message;
+    }
   }
   @Delete(':id')
-  deleteItem(@Param('id') id: string): string {
-    return this.appService.deleteItem(id);
+  deleteItem(@Param('id') id: string): iUserWithId[] | string {
+    try {
+      return this.appService.deleteItem(id);
+    } catch (error) {
+      return (error as Error).message;
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,58 @@
 import { Injectable } from '@nestjs/common';
 
+interface iUser {
+  name: string;
+  description: string;
+}
+
+interface iUserWithId extends iUser {
+  id: number;
+}
+
+const fakeDataBase: iUserWithId[] = [];
+
 @Injectable()
 export class AppService {
-  getAllItem(): string {
-    return 'getItem';
+  getAllItem(): iUserWithId[] {
+    if (fakeDataBase.length === 0) throw new Error('The database is empty');
+
+    return fakeDataBase;
   }
-  createItem(obj: any): any {
-    return obj;
+
+  createItem(obj: iUser): iUserWithId[] {
+    if (!obj.name || !obj.description)
+      throw new Error('There are incomplete fields');
+
+    const newId: number =
+      fakeDataBase.length === 0
+        ? 1
+        : fakeDataBase[fakeDataBase.length - 1].id + 1;
+    fakeDataBase.push({ id: newId, ...obj });
+
+    return fakeDataBase;
   }
-  updateItem(id: string, obj: any): any {
-    return [id, obj];
+
+  updateItem(id: string, obj: iUser): iUserWithId[] {
+    if (!obj.name || !obj.description)
+      throw new Error('There are incomplete fields');
+    if (fakeDataBase.findIndex((el) => el.id === +id) === -1)
+      throw new Error('Such ID does not exist');
+
+    const indexEl = fakeDataBase.findIndex((el) => el.id === +id);
+
+    fakeDataBase[indexEl] = { ...fakeDataBase[indexEl], ...obj };
+
+    return fakeDataBase;
   }
-  deleteItem(id: string): string {
-    return `deleteItem ${id}`;
+
+  deleteItem(id: string): iUserWithId[] {
+    if (fakeDataBase.findIndex((el) => el.id === +id) === -1)
+      throw new Error('Such ID does not exist');
+
+    const indexEl = fakeDataBase.findIndex((el) => el.id === +id);
+
+    fakeDataBase.splice(indexEl, 1);
+
+    return fakeDataBase;
   }
 }
